1af646ca6652b19e7194b79f4763e72748a02981
Nicholas C. Zakas <nicholas@humanwhocodes.com>
2023-04-13 14:02:22 -0700

    chore: Cleanup dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,16 @@
 export default [
     {
-        input: 'src/bin.js',
-        external: [
-            'fs',
-            'path'
-        ],
+        input: "src/bin.js",
         output: [
             {
-                file: 'dist/bin.cjs.js',
-                format: 'cjs',
-                banner: '#!/usr/bin/env node\n'
+                file: "dist/bin.cjs.js",
+                format: "cjs",
+                banner: "#!/usr/bin/env node\n"
             },
             {
-                file: 'dist/bin.js',
-                format: 'esm',
-                banner: '#!/usr/bin/env node\n'
+                file: "dist/bin.js",
+                format: "esm",
+                banner: "#!/usr/bin/env node\n"
             }
         ]
     }
